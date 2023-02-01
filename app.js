7362a53e7eccdd33d2e855a6ca520879dbbf8dbc
leeseoshim <leeseoit@gmail.com>
2023-02-01 23:46:50 +0900

    그림판색 선택 및 그리기

---
+++ app.js
@@ -0,0 +1,76 @@
+const canvas = document.querySelector("canvas");
+const lineWidth = document.querySelector("input#line-width");
+const color = document.querySelector("#color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const modeBtn = document.getElementById("mode-btn");
+const context = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
+context.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling = false;
+
+function onMove(event) {
+  if (isPainting) {
+    context.lineTo(event.offsetX, event.offsetY);
+    context.stroke();
+    return;
+  }
+  context.beginPath();
+  context.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+}
+
+function onLineWidthChange(event) {
+  context.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  context.strokeStyle = event.target.value;
+  context.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  context.strokeStyle = colorValue;
+  context.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+function onModeClick() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if (isFilling) {
+    context.fillRect(0, 0, 800, 800);
+  }
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
